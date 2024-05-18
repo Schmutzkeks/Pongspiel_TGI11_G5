429ba7e7364bd222c29d45520cfbb3e781031561
Schmutzkeks <lenny.bartossek@gmail.com>
2024-05-18 20:53:35 +0200

    Revert "Test Player aenderung"
    This reverts commit a4efe2ae6044229391e16cb9ff84438ce5dfb9b9.

--- PongSpiel/src/actions/KeyHandler.java
+++ PongSpiel/src/actions/KeyHandler.java
@@ -20,36 +20,22 @@ public class KeyHandler implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-/*
+
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			gamelogic.keyLeftarrowpressed = true;
 		
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			gamelogic.keyRightarrowpressed = true;
 		}
-		*/
-		
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			gamelogic.keyUparrowpressed = true;
-		
-		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			gamelogic.keyDownarrowpressed = true;
-		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-/*
+
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			gamelogic.keyLeftarrowpressed = false;
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			gamelogic.keyRightarrowpressed = false;
-		}
-		*/
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			gamelogic.keyUparrowpressed = false;
-		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			gamelogic.keyDownarrowpressed = false;
 		}
 		
 	}
