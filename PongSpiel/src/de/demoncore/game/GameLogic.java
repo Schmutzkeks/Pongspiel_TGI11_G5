b1fcce262c3497ac72dc30fdf22a7c9aec1a1d52
Schmutzkeks <lenny.bartossek@gmail.com>
2024-05-29 18:51:13 +0200

    Bounce

--- PongSpiel/src/de/demoncore/game/GameLogic.java
+++ PongSpiel/src/de/demoncore/game/GameLogic.java
@@ -8,47 +8,73 @@ import de.demoncore.gameObjects.BeweglichesRechteck;
 import de.demoncore.gui.Gui;
 
 public class GameLogic {
-	
+
 	private Timer gameTimer;
 	public int screenwidth;
 	public int screenheight;
+	int ingamescrwidth = 786;
+	int ingamescrheight = 593 - 31;
 	public int speed;
 	public ArrayList<GameObject> spielObjekte;
-	
+
 	//public boolean keyLeftarrowpressed;
 	//public boolean keyRightarrowpressed;
 	public boolean keyUparrowpressed;
 	public boolean keyDownarrowpressed;
-	
+
+
+
+
 	public GameLogic() {
 		gameTimer = new Timer();
 		spielObjekte = new ArrayList<GameObject>();
-		
+
 		//keyLeftarrowpressed = false;
 		//keyRightarrowpressed = false;
 		keyUparrowpressed = false;
 		keyDownarrowpressed = false;
-		
+
 		// Objekte im Spiel:
-		BeweglichesRechteck beispielObjekt1 = new BeweglichesRechteck(400, 240, 20, 20);		//(posX, posY, breite, hoehe) 
-		spielObjekte.add(beispielObjekt1);
-		beispielObjekt1.richtung = 0; // Startrichtung
-		BeweglichesRechteck beispielObjekt2 = new BeweglichesRechteck(700, 240, 20, 80);	//(posX, posY, breite, hoehe) 
+		BeweglichesRechteck Ball = new BeweglichesRechteck(393, 240, 20, 20);		//(posX, posY, breite, hoehe) 
+		spielObjekte.add(Ball);
+		Ball.richtung = 0; // Startrichtung
+		BeweglichesRechteck beispielObjekt2 = new BeweglichesRechteck(716, 240, 20, 80);	//(posX, posY, breite, hoehe) 
 		spielObjekte.add(beispielObjekt2);
+
+		BeweglichesRechteck beispielObjekt3 = new BeweglichesRechteck(50, 240, 20, 80);	//(posX, posY, breite, hoehe) 
+		spielObjekte.add(beispielObjekt3);
+
+		Gamelogic2 Detection = new Gamelogic2();
+
+
 		
+
+
 		gameTimer.scheduleAtFixedRate(new TimerTask(){
 			@Override
 			public void run() {
 				// Laufende Ausführungen im Spiel:
-				beispielObjekt1.bouncebewegung();
+				Ball.bouncebewegung();
+
+
+
+
+				
+				
+
+				
 				/*
-				if (keyLeftarrowpressed) {
-					beispielObjekt2.positionX -= 1;
-					System.out.println(screenwidth);
-				} else if (keyRightarrowpressed) {
-					beispielObjekt2.positionX += 1;
+				if (Detection.isColliding(beispielObjekt1, beispielObjekt2)) {
+					Detection.Collision(beispielObjekt1, beispielObjekt2);
 				}
-				 */
+				if (Detection.isColliding(beispielObjekt1, beispielObjekt3)) {
+					Detection.Collision(beispielObjekt1, beispielObjekt3);
+				}
+*/
+				if (Ball.positionY < (ingamescrheight-50) && Ball.positionY > 30) {
+					beispielObjekt3.positionY = Ball.positionY - 30;
+				}
+
 
 				if (keyUparrowpressed) {
 					beispielObjekt2.positionY -= 1;
@@ -56,8 +82,24 @@ public class GameLogic {
 					beispielObjekt2.positionY += 1;
 				}
 				
+				// Check for collisions
+				
+				if (Detection.isCollidingLeftRight(Ball, beispielObjekt2)) {
+					Detection.CollisionLeftRight(Ball, beispielObjekt2);
+				}
+				if (Detection.isCollidingLeftRight(Ball, beispielObjekt3)) {
+					Detection.CollisionLeftRight(Ball, beispielObjekt3);
+				}
+				if (Detection.isCollidingTopBottom(Ball, beispielObjekt2)) {
+					Detection.CollisionTopBottom(Ball, beispielObjekt2);
+				}
+				if (Detection.isCollidingTopBottom(Ball, beispielObjekt3)) {
+					Detection.CollisionTopBottom(Ball, beispielObjekt3);
+				}
+				
+
 			}
 		}, 0, 5);
 	}
-	
+
 }
--- PongSpiel/src/de/demoncore/gameObjects/BeweglichesRechteck.java
+++ PongSpiel/src/de/demoncore/gameObjects/BeweglichesRechteck.java
@@ -1,11 +1,23 @@
 package de.demoncore.gameObjects;
 
 import de.demoncore.game.GameObject;
+import de.demoncore.game.Gamelogic2;
 
 public class BeweglichesRechteck extends GameObject {
 	
 	public byte richtung;
 	public int schritteInGleicherRichtung;
+	int ingamescrwidth = 786;
+	int ingamescrheight = 593 - 31;
+	double Speed;
+	public static int velX = 1;
+	public static int velY = 1;
+	
+	
+	
+	Gamelogic2 velLogik = new Gamelogic2();
+
+	
 
 	public BeweglichesRechteck(int posX, int posY, int breite, int hoehe) {
 		super(posX, posY, breite, hoehe);
@@ -17,7 +29,7 @@ public class BeweglichesRechteck extends GameObject {
 		positionY += y;
 	}
 	
-	
+	/*
 	public void automatischeKreisbewegung() {
 		if(richtung == 0) {
 			positionX += 1;
@@ -38,21 +50,32 @@ public class BeweglichesRechteck extends GameObject {
 			schritteInGleicherRichtung += 1;
 		}
 	}
+	*/
 	
 	public void bouncebewegung() {
-		if(richtung == 0) {
-			Vector2(1, 1);
-		} else if (richtung == 1) {
-			Vector2(-1, -1);
-		} 
-		if (schritteInGleicherRichtung > 75) {
-			richtung += 1;
-			if (richtung > 3) {
-				richtung = 0;
-			}
-			schritteInGleicherRichtung = 0;
-		} else {
-			schritteInGleicherRichtung += 1;
-		}
+		
+		// tan for the bounce. tan = g/a = velY/velX
+		Vector2(velX, velY);
+
+
+
+        // Check for collision with wall
+        if (positionX <= 0 || positionX >= 786 - 20) {
+            if (velX>0&&velY>0) velX = velX *(-1);
+            else if (velX>0&&velY<0) velX = velX *(-1);
+            else if (velX<0&&velY>0) velX = velX *(-1);
+            else if (velX<0&&velY<0) velX = velX *(-1);
+        }
+
+        if (positionY <= 0 || positionY >= 562 - 20) {
+            if (velX>0&&velY>0) velY = velY *(-1);
+            else if (velX>0&&velY<0) velY = velY *(-1);
+            else if (velX<0&&velY>0) velY = velY *(-1);
+            else if (velX<0&&velY<0) velY = velY *(-1);
+        }
+		
+        
 	}
+	
+
 }
